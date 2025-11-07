96a5a3f603218495124e4eab8aed13605321110f
Jon <jon@jonhilton.net>
2025-11-07 17:22:36 +0000

    Add visibility change handling to pause and resume Blazor circuit

---
+++ BestPartsDemo/wwwroot/demo.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('visibilitychange', ()=> {
+    if(document.hidden) {
+        console.debug("Document hidden - pausing Blazor");
+        Blazor.pauseCircuit();
+    } else {
+        console.debug("Document visible - resuming Blazor");
+        Blazor.resumeCircuit();
+    }
+});
