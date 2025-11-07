ec5999c331407a4c850dadb0fa9aa4209e86f5e7
Jon <jon@jonhilton.net>
2025-11-07 16:36:00 +0000

    Refactor AllContacts component to use SortButton for sorting and improve loading state management

---
+++ BestPartsDemo/Components/Layout/CustomNavLink.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace BestPartsDemo.Components.Layout;
+
+public class CustomNavLink : NavLink
+{
+    protected override bool ShouldMatch(string uriAbsolute)
+    {
+        return uriAbsolute.EndsWith("bob");
+    }
+}
